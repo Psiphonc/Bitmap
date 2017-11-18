c4d4064ceff5e2f5eed5c4b1c3527c296c3eab65
psiphonc <psiphonc@outlook.com>
2017-11-18 21:22:44 +0800

    add basic function

--- Bitmap/Bitmap/Bitmap.h
+++ Bitmap/Bitmap/Bitmap.h
@@ -14,10 +14,35 @@ public:
 		fread(M, sizeof(char), N, fp);
 		fclose(fp);
 	}
+	void set(int k)
+	{
+		expand(k);
+		M[k >> 3] |= (0x80 >> (0x07 & k));
+	}
+	void clear(int k)
+	{
+		expand(k);
+		M[k >> 3] &= ~(0x80 >> (0x07 & k));
+	}
+	bool test(int k)
+	{
+		expand(k);
+			return M[k >> 3] & (0x80 >> (0x07 & k));
+	}
 private:
 	char *M;
 	int N;
 protected:
 	void init(int n) { M = new char[N = (n + 7) >> 3]; memset(M, 0, N); }
+	void expand(int k)
+	{
+		if (k < N << 3)
+			return;
+		char *old_elem = M;
+		int old_N = N;
+		init(k << 1);
+		memcpy_s(M,N,old_elem,old_N);
+		delete []old_elem;
+	}
 };
 
